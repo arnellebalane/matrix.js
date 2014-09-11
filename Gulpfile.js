061fc3060debfee54d6cf789ac1c61665081a643
Arnelle Balane <arnellebalane@gmail.com>
2014-09-11 09:14:40 +0800

    setup gulp for tests and better jshint

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,15 +2,21 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
 var uglify = require('gulp-uglify');
+var mocha = require('gulp-mocha');
 
 gulp.task('jshint', function() {
-    gulp.src(['src/**/*.js', 'Gulpfile.js'])
+    return gulp.src(['src/**/*.js', 'tests/**/*.js', 'Gulpfile.js'])
         .pipe(jshint())
         .pipe(jshint.reporter(stylish));
 });
 
 gulp.task('compress', function() {
-    gulp.src('src/**/*.js')
+    return gulp.src('src/**/*.js')
         .pipe(uglify())
         .pipe(gulp.dest('build'));
+});
+
+gulp.task('test', function() {
+    return gulp.src('tests/**/*.js', { read: false })
+        .pipe(mocha());
 });
