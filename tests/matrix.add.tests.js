8ba61e935e185327204bec4822ee7a7e2ca91118
Arnelle Balane <arnellebalane@gmail.com>
2014-09-11 18:06:02 +0800

    corrections in tests for matrix.add

--- tests/matrix.add.tests.js
+++ tests/matrix.add.tests.js
@@ -2,7 +2,7 @@ var matrix = require('../src/matrix').matrix;
 var assert = require('assert');
 
 describe('matrix.add', function() {
-    it('should perform matrix addition if given two matrices of the same dimensions', function() {
+    it('should perform matrix addition when given two matrices with the same dimensions', function() {
         var a = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
         var b = [[9, 8, 7], [6, 5, 4], [3, 2, 1]];
         var expected = [[10, 10, 10], [10, 10, 10], [10, 10, 10]];
@@ -10,7 +10,7 @@ describe('matrix.add', function() {
         assert.deepEqual(expected, actual);
     });
 
-    it('should throw MatrixError if given two matrices with different dimensions', function() {
+    it('should throw MatrixError when given two matrices with different dimensions', function() {
         var a = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
         var b = [[1, 2], [3, 4], [5, 6]];
         assert.throws(function() {
@@ -18,23 +18,23 @@ describe('matrix.add', function() {
         }, matrix.MatrixError, 'Matrices do not have the same dimensions.');
     });
 
-    it('should perform scalar addition if given a matrix and a scalar value', function() {
+    it('should perform scalar addition when given a matrix and a scalar value', function() {
         var a = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
         var b = 2;
-        var expected = [[2, 4, 6], [8, 10, 12], [14, 16, 18]];
+        var expected = [[3, 4, 5], [6, 7, 8], [9, 10, 11]];
         var actual = matrix.add(a, b);
         assert.deepEqual(expected, actual);
     });
 
-    it('should perform scalar addition if given a scalar value and a matrix', function() {
+    it('should perform scalar addition when given a scalar value and a matrix', function() {
         var a = 2;
         var b = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
-        var expected = [[2, 4, 6], [8, 10, 12], [14, 16, 18]];
+        var expected = [[3, 4, 5], [6, 7, 8], [9, 10, 11]];
         var actual = matrix.add(a, b);
         assert.deepEqual(expected, actual);
     });
 
-    it('should throw MatrixError if given two scalar values', function() {
+    it('should throw MatrixError when not given any matrices', function() {
         var a = 2;
         var b = 3;
         assert.throws(function() {
