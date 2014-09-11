44ea9b1123973ac781d565abc7d27aafb70b7f6c
Arnelle Balane <arnellebalane@gmail.com>
2014-09-11 08:40:32 +0800

    setup gulp tasks

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,1 +1,16 @@
 var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish');
+var uglify = require('gulp-uglify');
+
+gulp.task('jshint', function() {
+    gulp.src(['src/**/*.js', 'Gulpfile.js'])
+        .pipe(jshint())
+        .pipe(jshint.reporter(stylish));
+});
+
+gulp.task('compress', function() {
+    gulp.src('src/**/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('build'));
+});
