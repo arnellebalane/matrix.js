c157f550387104f8e172d260062ebd79a7945864
Arnelle Balane <arnellebalane@gmail.com>
2015-07-24 01:20:00 +0800

    implemented matrix.add

--- src/matrix.js
+++ src/matrix.js
@@ -16,6 +16,59 @@
 
 
     /**
+     *  Performs matrix addition on given matrices.
+     */
+    function _matrixAddition(a, b) {
+        var result = [];
+        for (var i = 0; i < a.length; i++) {
+            result.push([]);
+            for (var j = 0; j < b.length; j++) {
+                result[i].push(a[i][j] + b[i][j]);
+            }
+        }
+        return result;
+    }
+
+
+    /**
+     *  Performs scalar addition on the given matrix and scalar value.
+     */
+    function _scalarAddition(matrix, scalar) {
+        var result = [];
+        for (var i = 0; i < matrix.length; i++) {
+            result.push([]);
+            for (var j = 0; j < matrix[i].length; j++) {
+                result[i].push(matrix[i][j] + scalar);
+            }
+        }
+        return result;
+    }
+
+
+    /**
+     *  Performs matrix when given two matrices, or scalar addition when given
+     *  a matrix and a scalar value.
+     */
+    function add(a, b) {
+        if (a instanceof Array && b instanceof Array) {
+            if (!valid(a) || !valid(b)) {
+                throw new MatrixError('A given matrix is invalid.');
+            } else if (a.length !== b.length || a[0].length !== b[0].length) {
+                throw new MatrixError('Matrices are not of the same '
+                    + 'dimensions.');
+            }
+            return _matrixAddition(a, b);
+        } else if ((typeof a === 'number' && b instanceof Array)
+                || (typeof b === 'number' && a instanceof Array)) {
+            var matrix = a instanceof Array ? a : b;
+            var scalar = a instanceof Array ? b : a;
+            return _scalarAddition(matrix, scalar);
+        }
+        throw new MatrixError('No matrices given.');
+    }
+
+
+    /**
      *  Accepts a matrix and returns the transpose of the
      *  given matrix.
      */
@@ -100,7 +153,7 @@
 
 
     return {
-        add: null,
+        add: add,
         multiply: null,
         transpose: transpose,
         inverse: null,
