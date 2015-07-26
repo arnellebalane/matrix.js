555ea46eb66b0a6807655cd85d0b45317675d5a1
Arnelle Balane <arnellebalane@gmail.com>
2015-07-26 20:25:05 +0800

    added gulp-plumber to deps

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,11 +4,13 @@ import jscs from 'gulp-jscs';
 import stylish from 'gulp-jscs-stylish';
 import uglify from 'gulp-uglify';
 import babel from 'gulp-babel';
+import plumber from 'gulp-plumber';
 import gutil from 'gulp-util';
 
 
 gulp.task('lint', () => {
     return gulp.src(['src/**/*.js', 'tests/**/*.js', 'gulpfile.js'])
+        .pipe(plumber())
         .pipe(jshint())
         .pipe(jscs())
         .on('error', gutil.noop)
@@ -20,6 +22,7 @@ gulp.task('lint', () => {
 
 gulp.task('compile', () => {
     return gulp.src(['src/**/*.js', 'tests/**/*.js'], { base: '.' })
+        .pipe(plumber())
         .pipe(babel())
         .pipe(uglify())
         .pipe(gulp.dest('build'));
