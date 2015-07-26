f61cecca0c3588873b4156f207436310a24c1606
Arnelle Balane <arnellebalane@gmail.com>
2015-07-26 20:13:46 +0800

    converted tests for matrix.identity to es2015

--- tests/matrix.identity.js
+++ tests/matrix.identity.js
@@ -1,38 +1,39 @@
-var matrix = require('../src/matrix').matrix;
-var assert = require('assert');
+import { identity, MatrixError } from '../src/matrix';
+import assert from 'assert';
 
-describe('matrix.identity', function() {
+
+describe('matrix.identity', () => {
     it('should return an empty matrix when given zero for its dimensions',
-        function() {
+        () => {
             var expected = [];
-            var actual = matrix.identity(0);
+            var actual = identity(0);
             assert.deepEqual(expected, actual);
         }
     );
 
-    it('should return an n-by-n identity matrix when given a positive '
-        + 'integer n for its dimensions', function() {
+    it('should return an n-by-n identity matrix when given a positive integer'
+        + 'n for its dimensions', () => {
         var expected = [[1, 0, 0], [0, 1, 0], [0, 0, 1]];
-        var actual = matrix.identity(3);
+        var actual = identity(3);
         assert.deepEqual(expected, actual);
     });
 
     it('should throw MatrixError when given a negative integer for its '
-        + 'dimensions', function() {
-        assert.throws(function() {
-            matrix.identity(-3);
-        }, matrix.MatrixError, 'Invalid matrix dimensions given.');
+        + 'dimensions', () => {
+        assert.throws(() => {
+            identity(-3);
+        }, MatrixError, 'Invalid matrix dimensions given.');
     });
 
     it('should throw MatrixError when not given an integer for its dimensions',
-        function() {
-            assert.throws(function() {
-                matrix.identity('a');
-            }, matrix.MatrixError, 'Invalid matrix dimensions given.');
+        () => {
+            assert.throws(() => {
+                identity('a');
+            }, MatrixError, 'Invalid matrix dimensions given.');
         }
     );
 
-    it('should be integrated to Array.prototype', function() {
+    it('should be integrated to Array.prototype', () => {
         var expected = true;
         var actual = Array.prototype.hasOwnProperty('identity');
         assert.equal(expected, actual);
