307d6cb856bf15bef6b51e34b3662cb1546d0a59
Arnelle Balane <arnellebalane@gmail.com>
2015-07-26 20:38:36 +0800

    convert tests for matrix.zeros to es2015

--- tests/matrix.zeros.js
+++ tests/matrix.zeros.js
@@ -1,40 +1,33 @@
-var matrix = require('../src/matrix').matrix;
-var assert = require('assert');
+import { zeros, MatrixError } from '../src/matrix';
+import assert from 'assert';
 
 
-describe('matrix.zeros', function() {
-    it('should return a square matrix when given only one argument',
-        function() {
-            var expected = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
-            var actual = matrix.zeros(3);
-            assert.deepEqual(expected, actual);
-        }
-    );
+describe('matrix.zeros', () => {
+    it('should return a square matrix when given only one argument', () => {
+        var expected = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+        var actual = zeros(3);
+        assert.deepEqual(expected, actual);
+    });
 
     it('should return a matrix of correct dimensions when given two arguments',
-        function() {
+        () => {
             var expected = [[0, 0], [0, 0], [0, 0]];
-            var actual = matrix.zeros(3, 2);
+            var actual = zeros(3, 2);
             assert.deepEqual(expected, actual);
         }
     );
 
-    it('should throw TypeError when not given any arguments',
-        function() {
-            assert.throws(function() {
-                matrix.zeros();
-            }, TypeError, 'Matrix dimensions should be integers.');
-        }
-    );
+    it('should throw TypeError when not given any arguments', () => {
+        assert.throws(() => zeros(), TypeError,
+            'Matrix dimensions should be integers.');
+    });
 
     it('should throw TypeError when any given argument is not a number',
-        function() {
-            assert.throws(function() {
-                matrix.zeros('string');
-            }, TypeError, 'Matrix dimensions should be integers.');
-            assert.throws(function() {
-                matrix.zeros(3, 'string');
-            }, TypeError, 'Matrix dimensions should be integers.');
+        () => {
+            assert.throws(() => zeros('string'), TypeError,
+                'Matrix dimensions should be integers.');
+            assert.throws(() => zeros(3, 'string'), TypeError,
+                'Matrix dimensions should be integers.');
         }
     );
 });
