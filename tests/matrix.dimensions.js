5358ea2953d3eeb3f64a4b25ff44deec0aef0523
Arnelle Balane <arnellebalane@gmail.com>
2015-07-26 20:10:59 +0800

    converted tests for matrix.dimensions to es2015

--- tests/matrix.dimensions.js
+++ tests/matrix.dimensions.js
@@ -1,30 +1,30 @@
-var matrix = require('../src/matrix').matrix;
-var assert = require('assert');
+import { dimensions, MatrixError } from '../src/matrix';
+import assert from 'assert';
 
 
-describe('matrix.dimensions', function() {
-    it('should return the dimensions of an empty matrix', function() {
+describe('matrix.dimensions', () => {
+    it('should return the dimensions of an empty matrix', () => {
         var a = [];
         var expected = { rows: 0, columns: 0 };
-        var actual = matrix.dimensions(a);
+        var actual = dimensions(a);
         assert.deepEqual(expected, actual);
     });
 
-    it('should return the dimensions of a valid matrix', function() {
+    it('should return the dimensions of a valid matrix', () => {
         var a = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
         var expected = { rows: 3, columns: 3 };
-        var actual = matrix.dimensions(a);
+        var actual = dimensions(a);
         assert.deepEqual(expected, actual);
     });
 
-    it('should throw MatrixError when given an invalid matrix', function() {
+    it('should throw MatrixError when given an invalid matrix', () => {
         var a = [[1, 2, 3], [4, 5], [6, 7, 8]];
-        assert.throws(function() {
-            matrix.dimensions(a);
-        }, matrix.MatrixError, 'Given matrix is invalid.');
+        assert.throws(() => {
+            dimensions(a);
+        }, MatrixError, 'Given matrix is invalid.');
     });
 
-    it('should be integated to Array.prototype', function() {
+    it('should be integated to Array.prototype', () => {
         var expected = true;
         var actual = Array.prototype.hasOwnProperty('dimensions');
         assert.equal(expected, actual);
